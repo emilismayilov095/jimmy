375dba70d68ec441538a228842f31c7e8a77fb22
Emil Ismayilov <emilismayilov095@gmail.com>
2023-05-29 00:42:57 +0400

    initial commit

---
+++ Sources/jimmy/jimmy.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+#if canImport(UIKit)
+
+public struct Jimmy {
+    public init() { }
+}
+
+extension Jimmy {
+    public static func delayAfter(seconds delay: Double, closure: @escaping () -> ()) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
+            closure()
+        }
+    }
+}
+
+#endif
