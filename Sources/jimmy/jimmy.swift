286f81ef94df3b52c953f7468676663a462c9ee5
Emil Ismayilov <emilismayilov095@gmail.com>
2023-05-29 01:49:20 +0400

    show Alert Added

--- Sources/jimmy/jimmy.swift
+++ Sources/jimmy/jimmy.swift
@@ -1,6 +1,6 @@
-import Foundation
-
 #if canImport(UIKit)
+import Foundation
+import UIKit
 
 public struct Jimmy {
     public init() { }
@@ -14,4 +14,13 @@ extension Jimmy {
     }
 }
 
+extension Jimmy {
+    public static func showAlertWith(title: String, message: String, _ controller: UIViewController) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alert.addAction(action)
+        controller.present(alert, animated: true, completion: nil)
+    }
+}
+    
 #endif
